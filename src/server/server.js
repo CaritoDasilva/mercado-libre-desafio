8bb1276766b754a723323f387b46044a710b372d
CaritoDasilva <caritodasilva@gmail.com>
2019-06-27 02:15:06 -0400

    deplloy cors}

--- src/server/server.js
+++ src/server/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(cors)
+app.use(cors())
 
 app.get('/products/:product', async (req, res) => {
     try {
