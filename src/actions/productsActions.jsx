c1ef02c8816255812948741b7baea8ce37c48a7d
CaritoDasilva <caritodasilva@gmail.com>
2019-06-27 01:21:12 -0400

    deplloy 01:21

--- src/actions/productsActions.jsx
+++ src/actions/productsActions.jsx
@@ -1,11 +1,21 @@
 import { GET_PRODUCTS, SHOW_PRODUCTS } from './types'
-import { dispatch } from 'rxjs/internal/observable/range';
+import axios from 'axios';
 
+
+const proxyurl = 'https://cors-anywhere.herokuapp.com/'
 export const getProducts = (product) => async dispatch => {
     try {
         debugger
-        const url = `http://localhost:8080/products/${product}`
-        const response = await fetch(url);
+        const url = `https://localhost:8080/products/${product}`
+        const response = await axios({
+            method: 'get',
+            baseURL: url,
+
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
         const responseJson = await response.json()
         console.log(responseJson)
         dispatch({
@@ -19,12 +29,34 @@ export const getProducts = (product) => async dispatch => {
 }
 
 export const showProducts = () => async dispatch => {
-    const url = `http://localhost:8080/products/mesa`
-    const response = await fetch(url);
-    const responseJson = await response.json()
-    console.log(responseJson)
-    dispatch({
-        type: GET_PRODUCTS,
-        payload: responseJson
-    })
+    try {
+        debugger
+        const url = `http://localhost:8080/products/mesa`
+        const response = await axios({
+            method: 'get',
+            baseURL: url,
+
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+            // url: '/login/authentication',
+            // data: {
+            //     email: email,
+            //     password: password
+            // }
+        })
+
+
+        const { data } = await response
+        console.log(data)
+        dispatch({
+            type: SHOW_PRODUCTS,
+            payload: data
+        })
+    }
+    catch (error) {
+        console.log(error)
+    }
 }
+
